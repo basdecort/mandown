77f2fcaa681e990bdf4096b7b1abe05a282b8847
Bas de Cort <bas.decort@infosupport.com>
2017-05-09 13:26:25 -0700

    Clean website

---
+++ ManDown/ManDown/Areas/HelpPage/ModelDescriptions/DictionaryModelDescription.cs
@@ -0,0 +1,6 @@
+namespace ManDown.Areas.HelpPage.ModelDescriptions
+{
+    public class DictionaryModelDescription : KeyValuePairModelDescription
+    {
+    }
+}
---
+++ ManDown/ManDown/Areas/HelpPage/ModelDescriptions/SimpleTypeModelDescription.cs
@@ -0,0 +1,6 @@
+namespace ManDown.Areas.HelpPage.ModelDescriptions
+{
+    public class SimpleTypeModelDescription : ModelDescription
+    {
+    }
+}
